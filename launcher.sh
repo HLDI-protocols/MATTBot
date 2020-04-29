569a542cb0b8d6a8bfb1802f38e6b765f420f95e
Predator <45335553+HLDynamics@users.noreply.github.com>
2020-04-29 21:26:11 +0530

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 cd System
-konsole -e python3 MATTAutomata.py & 
-konsole -e python3 console.py &
+#konsole -e python3 MATTAutomata.py & 
+#konsole -e python3 console.py &
+gnome-terminal -e 'python3 MATTAutomata.py &'
+gnome-terminal -e 'python3 console.py &'
  
 
 
