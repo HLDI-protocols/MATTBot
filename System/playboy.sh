6976e586e1ff9e174e1f048360260f4a6843b1ab
Predator <45335553+HLDynamics@users.noreply.github.com>
2020-05-10 00:02:58 +0530

    update for Gnome

--- System/playboy.sh
+++ System/playboy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 echo MATTbot playing your music collection;
 cd MATT_PlayBoy
-konsole -e play *.mp3 &
+#konsole -e play *.mp3 &
+gnome-terminal -e 'play *.mp3 '	
+
--- launcher.sh
+++ launcher.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 cd System
 #KDE uncomment konsole
-konsole -e python3 MATTAutomata.py & 
-konsole -e python3 console.py &
+#konsole -e python3 MATTAutomata.py & 
+#konsole -e python3 console.py &
 #UBUNTU/other gnome distro uncomment gnome
-#gnome-terminal -e 'python3 MATTAutomata.py &'
-#gnome-terminal -e 'python3 console.py &'
+gnome-terminal -e 'python3 MATTAutomata.py &'
+gnome-terminal -e 'python3 console.py &'
 wait
 exit
 
