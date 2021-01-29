765d064a70b64d0f9bc01691e955d80d391ef753
HLDynamics <akhil.pj007@hotmail.com>
2021-01-29 22:48:32 +0530

    un necessary files removed

--- launcher.sh
+++ launcher.sh
@@ -1,9 +1,32 @@
-echo "[MATTBot 2020 version 2.0]";
+#!/bin/sh
+#gif-for-cli "Prop/HLD.gif"
+python3 -m gif_for_cli "Prop/HLD.gif"
 wait
-echo "[This software is MIT Licensed, Please read license agreement for more details]";
+#gif-for-cli "Prop/MATTBot.gif"
+python3 -m gif_for_cli "Prop/MATTBot.gif"
 wait
-echo "[Status:Started]";
-echo "[Status:Running]";
-wait
+
 cd System
-python3 MATTBOT_X.py
+#KDE uncomment konsole
+#konsole -e python3 MATTAutomata.py & 
+#konsole -e python3 console.py &
+#UBUNTU/other gnome distro uncomment gnome
+#gnome-terminal -e 'python3 MATTAutomata.py &'
+#gnome-terminal -e 'python3 console.py &'
+./OUL.sh
+wait
+cd ..
+clear
+echo "MATTBot 2020 activated"
+echo "designed and developed by Akhil P Jacob"
+echo "last updated on 12th AUGUST 2020"
+#gif-for-cli "Prop/HLD.gif"
+python3 -m gif_for_cli "Prop/MATTBot.gif"
+
+exit
+
+
+ 
+
+
+ 
